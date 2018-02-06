8b61fa5d89eef4b26c13bee59028462559ec660a
Landon Patmore <landon.patmore@gmail.com>
2018-02-05 23:26:06 -0500

    added routes, no tests yet

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
 const assert = require("assert");
+const httpMock = require("node-mocks-http");
+
 const val = require("./helpers/validation");
+
+// Routes
+const indexRoute = require("./routes/index");
+const validationRoute = require("./routes/validation_route");
+const wordsRoute = require("./routes/words");
 
 describe(".validation(typed_word)", function() {
     it("should return true for words in the dictionary", function(){
