cea9880315254d1753883a6f4f95a075dd25d9bc
Landon Patmore <landon.patmore@gmail.com>
2018-02-05 19:16:05 -0500

    triggering travis build

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const val = require("./helpers/validation");
 
 describe(".validation(typed_word)", function() {
-    it("returns true for words in the dictionary", function(){
+    it("should return true for words in the dictionary", function(){
         assert(val.validation("oswego") === true);
     });
 
